260ec40849ddd18c0f915d028366683199133111
TranManhCuong <cuongtm@gm-group.vn>
2024-03-26 12:13:47 +0700

    update

--- cat/src/AppTest.kt
+++ cat/src/AppTest.kt
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test
 
 class AppTest {
     @Test
-    fun `test something`() {
+    fun `test reading_file_with_number`() {
         val app = Cat()
         app.main(arrayOf("-tn", "-A", "data/files.txt", "data/files.txt"))
     }
